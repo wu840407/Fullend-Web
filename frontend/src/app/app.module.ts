323e550a743202957c8487959b7c38b91eb0f797
ChengRung Wu <wu840407@gmail.com>
2023-07-05 14:41:05 +0800

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
-import { UserListComponent } from './userlist/userlist.component';
-import { UserFormComponent } from './userform/useform.component';
 import { UserformComponent } from './userform/userform.component';
 import { UserlistComponent } from './userlist/userlist.component';
 
@@ -17,8 +15,6 @@ import { UserlistComponent } from './userlist/userlist.component';
     HomeComponent,
     RegistrationComponent,
     LoginComponent,
-    UserListComponent,
-    UserFormComponent,
     UserformComponent,
     UserlistComponent
   ],
